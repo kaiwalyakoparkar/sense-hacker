b3debdafafe5279b954dd65db835c617494b808e
= <kaiwalyakoparkar@gmail.com>
2021-04-10 16:26:12 +0530

    Click handler for all player elements

--- sense-hack/scripts/script.js
+++ sense-hack/scripts/script.js
@@ -18,3 +18,14 @@ updateScores();
 document.querySelector(".game-board").classList.add("before-game");
 
 var allCardElements = document.querySelectorAll(".card");
+
+// Adds click handler to all player card elements
+for(var i = 0; i < allCardElements.length; i++) {
+  var card = allCardElements[i];
+  if(card.classList.contains("player-card")) {
+    card.addEventListener("click",function(e){
+      cardClicked(this);
+    });
+  }
+}
+
