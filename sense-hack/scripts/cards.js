be2cbd2024a88375cff73407a904b95f97f5fcfd
= <kaiwalyakoparkar@gmail.com>
2021-04-10 16:03:53 +0530

    hacker question 3 and user answer 3 added

--- sense-hack/scripts/cards.js
+++ sense-hack/scripts/cards.js
@@ -1,5 +1,6 @@
 var scenarios = [
     {
+    //One
     hackerCard : {
       description : "I set up a fake Wi-Fi station to steal people’s email and track them online.",
       power : 4,
@@ -19,6 +20,7 @@ var scenarios = [
       }
     ]
   },
+  //two
   {
     hackerCard : {
       description : "I sent a fake email from your bank asking for your account details.",
@@ -35,6 +37,27 @@ var scenarios = [
       },
       {
         description : "I sent the details you asked for so you could check on my account.",
+        power : 1,
+      }
+    ]
+  },
+  //three
+  {
+    hackerCard : {
+      description : "I figured out where you live from all the personal information you share on social media.",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I never share personal information on my social media accounts.",
+        power : 5,
+      },
+      {
+        description : "I keep my accounts private so only my friends can see them.",
+        power : 4,
+      },
+      {
+        description : "I tag everything so my friends always know what I’m doing.",
         power : 1,
       }
     ]
