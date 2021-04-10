981829a3063abbac768c8f9ede7ca9f4726b2b53
= <kaiwalyakoparkar@gmail.com>
2021-04-10 15:59:31 +0530

    user answer 1 added

--- sense-hack/scripts/cards.js
+++ sense-hack/scripts/cards.js
@@ -4,5 +4,19 @@ var scenarios = [
       description : "I set up a fake Wi-Fi station to steal people’s email and track them online.",
       power : 4,
     },
+    playerCards : [
+      {
+        description : "I never use public wifi networks.",
+        power : 5,
+      },
+      {
+        description : "I browse the web, but I never do any personal business on a public wifi network.",
+        power : 3,
+      },
+      {
+        description : "I connect to any wifi network I can use in public.",
+        power : 1,
+      }
+    ]
   },
 ]
