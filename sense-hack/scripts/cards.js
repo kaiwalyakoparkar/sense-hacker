f2bfbf80b09d04beec35d2c0a147674a18ba6f91
= <kaiwalyakoparkar@gmail.com>
2021-04-10 16:02:03 +0530

    hacker question 2 and user answers 2 added

--- sense-hack/scripts/cards.js
+++ sense-hack/scripts/cards.js
@@ -19,4 +19,24 @@ var scenarios = [
       }
     ]
   },
+  {
+    hackerCard : {
+      description : "I sent a fake email from your bank asking for your account details.",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I checked the email address - the message didn’t come from my bank.",
+        power : 5,
+      },
+      {
+        description : "I never give out personal information in response to an email.",
+        power : 4,
+      },
+      {
+        description : "I sent the details you asked for so you could check on my account.",
+        power : 1,
+      }
+    ]
+  },
 ]
