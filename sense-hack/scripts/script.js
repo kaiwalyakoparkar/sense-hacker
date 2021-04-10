c4901483a666cb85397eb712054a81d6bcd4e872
= <kaiwalyakoparkar@gmail.com>
2021-04-10 16:24:04 +0530

    winning message and start lifes added

--- sense-hack/scripts/script.js
+++ sense-hack/scripts/script.js
@@ -2,3 +2,10 @@
 var playerLife = 5;
 var hackerLife = 5;
 
+// Message when the game is over
+var hackerWinnerMessage = "Game over: You got hacked!";
+var playerWinnerMessage = "You defeated the hacker!";
+
+// Game code starts here
+var playerStartLife = parseInt(playerLife);
+var hackerStartLife = parseInt(hackerLife);
